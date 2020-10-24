f3a8f4287e5533e14e1c3ae12723aeda66c94cb2
Nathaniel Capule <nathaniel.capule@celconsulting.com>
2020-10-24 17:33:09 +0800

    feat(playground): Exploring event handling in svelte.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,9 +2,14 @@
   import Playground from './playground/Playground.svelte';
 
   export let name: string;
+
+  $: object = {
+    thing: `the powah is in yo <strong>hands</strong>!`,
+    name: name,
+  };
 </script>
 
-<style>
+<style type="text/scss">
   main {
     text-align: center;
     padding: 1em;
@@ -17,6 +22,10 @@
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
+
+    > input {
+      max-width: 5em;
+    }
   }
 
   @media (min-width: 640px) {
@@ -27,11 +36,11 @@
 </style>
 
 <main>
-  <h1>Hallo {name}!</h1>
+  <h1>Hallo <input type="text" bind:value={name} />!</h1>
   <p>
     Visit the
     <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
     to learn how to build Svelte apps.
   </p>
-  <Playground thing="the powah is in yo <strong>hands</strong>!" />
+  <Playground {...object} />
 </main>
--- frontend/src/playground/Playground.svelte
+++ frontend/src/playground/Playground.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
   export let thing = `oh no <strong>!!!!</strong>`;
+  export let name: string;
 
   let clicks = 0;
   let trapHistory = [];
+  let promise = until2secs();
+
+  async function until2secs(): Promise<number> {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    if (Math.random() < 0.5) throw new Error('ohshit');
+    return Math.random();
+  }
 
   function handleClick() {
     clicks += 1;
+    promise = until2secs();
   }
 
   $: trap = clicks / 2;
-  $: trapHistory = [...trapHistory, trap];
+  $: {
+    trapHistory = [...trapHistory, trap];
+    if (trapHistory.length > 5) {
+      trapHistory = trapHistory.slice(1);
+    }
+  }
   $: console.log(`hell naw it's a ${trapHistory}`);
 </script>
 
@@ -19,16 +33,47 @@
       color: red;
       background-color: yellow;
     }
+
+    > .html-test {
+      border: 1px solid blue;
+      > span {
+        display: block;
+      }
+    }
   }
 </style>
 
 <div>
-  <button on:click={handleClick}>Click me pls x{clicks}/{trap}</button>
-  <div>
-    <span>This the {thing}.</span>
-    <span>
-      But this is another
-      {@html thing}
-    </span>
+  <button on:click={handleClick}>Click me pls {name}: x{clicks}/{trap}</button>
+  {#await promise}
+    <p>waiting for promise</p>
+  {:then randomnumber}
+    <p>I got a {randomnumber}</p>
+  {:catch error}
+    <p>OHNO {error}</p>
+  {/await}
+  <div class="html-test">
+    {#if clicks % 5 === 0}
+      <span>Crooo... crooo...</span>
+    {:else if clicks % 2}
+      <span>This the first {thing}.</span>
+    {:else}
+      <span>
+        But this is another
+        {@html thing}
+      </span>
+    {/if}
   </div>
+  {#each trapHistory as trap, i (trap)}
+    <!-- Avoid distracting elements my ass. -->
+    <div>
+      <marquee>{trap} #{i}</marquee>
+    </div>
+  {/each}
+  {#each trapHistory as trap, i}
+    <!-- Avoid distracting elements my ass. -->
+    <div on:mousemove={(e) => handleClick()}>
+      <marquee>{trap} #{i}</marquee>
+    </div>
+  {/each}
 </div>
