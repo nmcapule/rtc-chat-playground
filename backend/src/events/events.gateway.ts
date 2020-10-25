67b42e70021e9e5f25b65138ae19be8bc5309434
Nathaniel Capule <nathaniel.capule@celconsulting.com>
2020-10-25 20:28:14 +0800

    feat(leaflet): Initial websockets integration.

--- backend/src/events/events.gateway.ts
+++ backend/src/events/events.gateway.ts
@@ -1,26 +1,32 @@
 import {
-    MessageBody,
-    SubscribeMessage,
-    WebSocketGateway,
-    WebSocketServer,
-    WsResponse,
-  } from '@nestjs/websockets';
-  import { from, Observable } from 'rxjs';
-  import { map } from 'rxjs/operators';
-  import { Server } from 'socket.io';
-  
-  @WebSocketGateway(null, {transports: ['websocket']})
-  export class EventsGateway {
-    @WebSocketServer()
-    server: Server;
-  
-    @SubscribeMessage('events')
-    findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-      return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
-    }
-  
-    @SubscribeMessage('identity')
-    async identity(@MessageBody() data: number): Promise<number> {
-      return data;
-    }
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Server } from 'socket.io';
+
+@WebSocketGateway(null, { transports: ['websocket'] })
+export class EventsGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('events')
+  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+    console.log(data);
+    return from([1, 2, 4, 3]).pipe(map((item) => ({ event: 'events', data: item + 2 })));
   }
+
+  // @SubscribeMessage('stubs')
+  // stubs(@MessageBody() data: any): Observable<WsResponse<string>> {
+
+  // }
+
+  @SubscribeMessage('identity')
+  async identity(@MessageBody() data: number): Promise<number> {
+    return data;
+  }
+}
