b36a465d26c5214fd5657b23ede6d817d2156072
Nathaniel M. Capule <nmcapule@gmail.com>
2020-10-31 08:56:10 +0000

    feat(socket): Add socket page.

---
+++ frontend/src/socket/Socket.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let users = ['hey', 'get', ' your ', 'game', 'on'];
+</script>
+
+<style lang="scss">
+  .socket-container {
+    color: #3a3a3a;
+  }
+</style>
+
+<div class="socket-container">
+  <ul>
+    {#each users as user}
+      <li class="entry">{user}</li>
+    {/each}
+  </ul>
+</div>
